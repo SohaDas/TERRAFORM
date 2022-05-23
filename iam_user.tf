bb6295018b5491dc3d72287e615b79527ec5fc7c
SohaDas <101792662+SohaDas@users.noreply.github.com>
2022-05-23 13:38:57 +0530

    Adding iam_user

---
+++ iam_user.tf
@@ -0,0 +1,12 @@
+resource "aws_iam_user" "USER12" {
+    name = "USER12"
+    tags = {
+        tag-key = "TAG"
+    }
+}
+resource "aws_iam_access_key" "USER12" {
+    user = aws_iam_user.USER12.name
+}
+
+
+
